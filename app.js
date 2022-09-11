8911b66fb18a35c1f97a3ac6b589d2e38b1f4fbc
Alima07 <alex.lima_dev@outlook.com>
2022-09-11 19:15:55 -0300

    Inclusao do redirecionamento para erro 404, pagina nao encontrada, inclusao de titulos para informar ao usuario de qual metodo se trata e sua funcao

--- app.js
+++ app.js
@@ -14,4 +14,9 @@ app.use(express.static("public"));
 const IndexRouter = require ('./routers/IndexRouter');
 app.use('/', IndexRouter);
 
+// Caso não seja encontrado nenhuma das páginas acima, aplicar redirecionamento para erro 404
+app.use((req, res, next) => {res.status(404).render('not-found.ejs') });
+
 app.listen(3000, ()=> {console.log('Server Runing Port 3000')});
+
+
